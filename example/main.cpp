7238b02eb69ee361377a1a6a2be829f4ac45f09a
timoxd7 <timoxd7@gmx.de>
2020-07-16 16:11:13 +0200

    Google Format Style

--- example/main.cpp
+++ example/main.cpp
@@ -1,19 +1,18 @@
 #include "../src/drawIt.h"
-
 #include "Arduino.h"
-#include "XPT2046_Touchscreen.h"
 #include "ILI9341_t3.h"
+#include "XPT2046_Touchscreen.h"
 
 // Modify the following lines to match your hardware
-#define TFT_DC   9
-#define TFT_CS   10
-#define TFT_RST  6
+#define TFT_DC 9
+#define TFT_CS 10
+#define TFT_RST 6
 #define TFT_MOSI 11
 #define TFT_MISO 12
-#define TFT_CLK  13
+#define TFT_CLK 13
 
 //And also the Touch-Pins for the XPT2046
-#define TOUCH_CS  8
+#define TOUCH_CS 8
 #define TOUCH_IRQ 2
 
 #define delayx 20
@@ -27,100 +26,99 @@ ILI9341_t3 display = ILI9341_t3(TFT_CS, TFT_DC, TFT_RST, TFT_MOSI, TFT_CLK, TFT_
 
 XPT2046_Touchscreen touch(TOUCH_CS, TOUCH_IRQ);
 
-
-const uint16_t slider_x_length =  50, slider_y_length = 310,
+const uint16_t slider_x_length = 50, slider_y_length = 310,
                button_x_length = 100, button_y_length = 100;
 
 drawIt::slider slider[2] = drawIt::slider(display);
 drawIt::button button[2] = drawIt::button(display);
 
 void setup() {
-  display.begin();
-  display.setRotation(0);
-  display.fillScreen(DRAWIT_WHITE);
+    display.begin();
+    display.setRotation(0);
+    display.fillScreen(DRAWIT_WHITE);
 
-  slider[0].changeOrigin(5, 5);
-  slider[1].changeOrigin(60, 5);
+    slider[0].changeOrigin(5, 5);
+    slider[1].changeOrigin(60, 5);
 
-  button[0].changeOrigin(115, 5);
-  button[1].changeOrigin(115, 110);
-
-  for (int i = 0; i < 2; i++) {
-    slider[i].changeLength(slider_x_length, slider_y_length);
-    slider[i].autoDraw(true);
-
-    button[i].changeLength(button_x_length, button_y_length);
-    button[i].autoDraw(true);
-  }
-
-  display.setTextColor(DRAWIT_BLACK);
-
-  touch.begin(display.width(), display.height(), touch.getEEPROMCalibration());
-
-  char buf[15];
-  for (int i = 0; i < 2; i++) {
-    snprintf(buf, sizeof(buf), "Slider %i at %i%%", i + 1, (uint8_t)(slider[i].getValue() * 100));
-    display.setCursor(115, 235 + (i * 10));
-    display.print(buf);
-  }
-  
-  display.setCursor(115, 265);
-  display.fillRect(110, 265, 130, 8, DRAWIT_WHITE);
-  display.print("Pressure 0");
-}
-
-void loop() {
-  static bool touched = true;
-
-  if (touch.touched()) {
-    touched = true;
-
-    TS_Point p = touch.getPixel();
+    button[0].changeOrigin(115, 5);
+    button[1].changeOrigin(115, 110);
 
     for (int i = 0; i < 2; i++) {
-      if (slider[i].touched(p.x, p.y)) {
-        display.fillRect(110, 215, 130, 8, DRAWIT_WHITE);
-        display.fillRect(110, 235 + (i * 10), 130, 8, DRAWIT_WHITE);
+        slider[i].changeLength(slider_x_length, slider_y_length);
+        slider[i].autoDraw(true);
 
-        char buf[17];
+        button[i].changeLength(button_x_length, button_y_length);
+        button[i].autoDraw(true);
+    }
 
-        snprintf(buf, sizeof(buf), "Slider %i touched", i + 1);
-        display.setCursor(115, 215);
-        display.print(buf);
+    display.setTextColor(DRAWIT_BLACK);
 
+    touch.begin(display.width(), display.height(), touch.getEEPROMCalibration());
+
+    char buf[15];
+    for (int i = 0; i < 2; i++) {
         snprintf(buf, sizeof(buf), "Slider %i at %i%%", i + 1, (uint8_t)(slider[i].getValue() * 100));
         display.setCursor(115, 235 + (i * 10));
         display.print(buf);
-      }
     }
 
-    for (int i = 0; i < 2; i++) {
-      if (button[i].touched(p.x, p.y)) {
-        display.fillRect(110, 215, 130, 8, DRAWIT_WHITE);
-
-        char buf[17];
-        snprintf(buf, sizeof(buf), "Button %i touched", i + 1);
-        display.setCursor(115, 215);
-        display.print(buf);
-      }
-    }
-
-    char buf[14];
-    snprintf(buf, sizeof(buf), "Pressure %i", p.z);
     display.setCursor(115, 265);
     display.fillRect(110, 265, 130, 8, DRAWIT_WHITE);
-    display.print(buf);
-
-    delay(delayx);
-  } else if (touched) {
-    touched = false;
-
-    display.fillRect(110, 215, 130, 8, DRAWIT_WHITE);
-    display.setCursor(115, 215);
-    display.print("Nothing touched");
-    
-    display.fillRect(110, 265, 130, 8, DRAWIT_WHITE);
-    display.setCursor(115, 265);
     display.print("Pressure 0");
-  }
+}
+
+void loop() {
+    static bool touched = true;
+
+    if (touch.touched()) {
+        touched = true;
+
+        TS_Point p = touch.getPixel();
+
+        for (int i = 0; i < 2; i++) {
+            if (slider[i].touched(p.x, p.y)) {
+                display.fillRect(110, 215, 130, 8, DRAWIT_WHITE);
+                display.fillRect(110, 235 + (i * 10), 130, 8, DRAWIT_WHITE);
+
+                char buf[17];
+
+                snprintf(buf, sizeof(buf), "Slider %i touched", i + 1);
+                display.setCursor(115, 215);
+                display.print(buf);
+
+                snprintf(buf, sizeof(buf), "Slider %i at %i%%", i + 1, (uint8_t)(slider[i].getValue() * 100));
+                display.setCursor(115, 235 + (i * 10));
+                display.print(buf);
+            }
+        }
+
+        for (int i = 0; i < 2; i++) {
+            if (button[i].touched(p.x, p.y)) {
+                display.fillRect(110, 215, 130, 8, DRAWIT_WHITE);
+
+                char buf[17];
+                snprintf(buf, sizeof(buf), "Button %i touched", i + 1);
+                display.setCursor(115, 215);
+                display.print(buf);
+            }
+        }
+
+        char buf[14];
+        snprintf(buf, sizeof(buf), "Pressure %i", p.z);
+        display.setCursor(115, 265);
+        display.fillRect(110, 265, 130, 8, DRAWIT_WHITE);
+        display.print(buf);
+
+        delay(delayx);
+    } else if (touched) {
+        touched = false;
+
+        display.fillRect(110, 215, 130, 8, DRAWIT_WHITE);
+        display.setCursor(115, 215);
+        display.print("Nothing touched");
+
+        display.fillRect(110, 265, 130, 8, DRAWIT_WHITE);
+        display.setCursor(115, 265);
+        display.print("Pressure 0");
+    }
 }
--- src/drawIt.h
+++ src/drawIt.h
@@ -5,7 +5,7 @@
 */
 
 //#define _displayObjectType TFT_ILI9341      //For the ILI9341 optimised libary ONLY FOR AVR PROCESSORS (by Bodmer)
-#define _displayObjectType ILI9341_t3         //For the obtimised ILI9341 library for the teensy (by Paul Stoffregen)
+#define _displayObjectType ILI9341_t3  //For the obtimised ILI9341 library for the teensy (by Paul Stoffregen)
 //#define _displayObjectType Adafruit_ILI9341 //For the original Adafruit_ILI9341 library
 
 //#define _displayObjectType   //Or just put in our own
@@ -18,7 +18,7 @@
 */
 
 //#include "TFT_ILI9341.h" //For the ILI9341 optimised libary ONLY FOR AVR PROCESSORS (by Bodmer)
-#include "ILI9341_t3.h"    //For the obtimised ILI9341 library [for the teensy 3.2] (by Paul Stoffregen)
+#include "ILI9341_t3.h"  //For the obtimised ILI9341 library [for the teensy 3.2] (by Paul Stoffregen)
 
 //#include "Adafruit_GFX.h"
 //#include "Adafruit_ILI9341.h" //These both for the original Adafruit GFX library with ILI9341 display
@@ -31,9 +31,6 @@
 
 
 
-
-
-
 //-----------------------------------------------------------------------------------------------------
 
 //User Errors
@@ -42,7 +39,7 @@
 
 #ifndef _displayObjectType
 #error "You have to specify an object-type in the drawIt.h !"
-#endif //_displayObjectType
+#endif  //_displayObjectType
 
 
 //-----------------------------------------------------------------------------------------------------
@@ -57,15 +54,15 @@
 #include "Arduino.h"
 #include "SPI.h"
 
-#define DRAWIT_BLACK    0x0000
-#define DRAWIT_BLUE     0x001F
-#define DRAWIT_RED      0xF800
-#define DRAWIT_GREEN    0x07E0
-#define DRAWIT_CYAN     0x07FF
-#define DRAWIT_MAGENTA  0xF81F
-#define DRAWIT_YELLOW   0xFFE0
-#define DRAWIT_WHITE    0xFFFF
-#define DRAWIT_GREY     0x5AEB
+#define DRAWIT_BLACK 0x0000
+#define DRAWIT_BLUE 0x001F
+#define DRAWIT_RED 0xF800
+#define DRAWIT_GREEN 0x07E0
+#define DRAWIT_CYAN 0x07FF
+#define DRAWIT_MAGENTA 0xF81F
+#define DRAWIT_YELLOW 0xFFE0
+#define DRAWIT_WHITE 0xFFFF
+#define DRAWIT_GREY 0x5AEB
 
 
 //-----------------------------------------------------------------------------------------------------
@@ -74,37 +71,33 @@
 
 //-----------------------------------------------------------------------------------------------------
 
-class drawIt
-{
-  private:
-
+class drawIt {
+   private:
     class visuals {
-      public:
+       public:
+        void changeOrigin(uint16_t x_origin, uint16_t y_origin);  //
+        void changeLength(uint16_t x_length, uint16_t y_length);  //
 
-        void changeOrigin(uint16_t x_origin, uint16_t y_origin); //
-        void changeLength(uint16_t x_length, uint16_t y_length); //
-
-        void setVisibility(bool visible); //
+        void setVisibility(bool visible);  //
 
         bool getVisibility() {
-          return _visible;
+            return _visible;
         }
 
         void autoDraw(bool active);
         bool autoDraw() {
-          return _autoDraw;
+            return _autoDraw;
         }
-        
+
         virtual void draw();
 
-      protected:
-
+       protected:
         struct _origin {
-          uint16_t x, y;
+            uint16_t x, y;
         } _origin;
 
         struct _length {
-          uint16_t x, y;
+            uint16_t x, y;
         } _length;
 
         bool _visible, _autoDraw;
@@ -112,55 +105,51 @@ class drawIt
 
 
     class touch {
-      public:
+       public:
+        virtual bool touched(uint16_t x, uint16_t y);  //
 
-        virtual bool touched(uint16_t x, uint16_t y); //
-
-        void setTouch(bool activated){
-          _touch = activated;
+        void setTouch(bool activated) {
+            _touch = activated;
         }
 
-        bool getTouch(){
-          return _touch;
+        bool getTouch() {
+            return _touch;
         }
 
-      protected:
-
+       protected:
         bool _touch;
     };
 
 
     class color {
-      public:
-
+       public:
         void setBackgroundColor(uint16_t backgroundColor);
 
         uint16_t getBackgroundColor() {
-          return _color.background;
+            return _color.background;
         }
 
         void setOutlineColor(uint16_t outlineColor);
 
         uint16_t getOutlineColor() {
-          return _color.outline;
+            return _color.outline;
         }
 
-      protected:
+       protected:
         struct _color {
-          uint16_t background = DRAWIT_WHITE, outline = DRAWIT_BLACK;
+            uint16_t background = DRAWIT_WHITE, outline = DRAWIT_BLACK;
         } _color;
     };
 
 
-  public:
-
+   public:
     class slider : public visuals, public touch, public color {
-      public:
-        slider(_displayObjectType& _display, uint16_t x_origin = 0, uint16_t y_origin = 0, uint16_t x_length = 0, uint16_t y_length = 0, bool autoDrawActivated = false, float value = 0.0, bool touchActivated = true); //
+       public:
+        slider(_displayObjectType& _display, uint16_t x_origin = 0, uint16_t y_origin = 0, uint16_t x_length = 0, uint16_t y_length = 0, bool autoDrawActivated = false, float value = 0.0, bool touchActivated = true);  //
 
-        void setValue(float value); //
+        void setValue(float value);  //
 
-        float getValue(); //
+        float getValue();  //
 
         bool touched(uint16_t x, uint16_t y);
 
@@ -168,27 +157,27 @@ class drawIt
 
         uint16_t getSliderBackgroundColor();
 
-        void draw(); //
+        void draw();  //
 
-      protected:
+       protected:
         _displayObjectType& _display;
 
         float _value;
 
         struct _sliderColor {
-          uint16_t background = DRAWIT_WHITE, outline = DRAWIT_BLACK;
+            uint16_t background = DRAWIT_WHITE, outline = DRAWIT_BLACK;
         } _sliderColor;
     };
 
 
     class button : public visuals, public touch, public color {
-      public:
+       public:
         button(_displayObjectType& _display, uint16_t x_origin = 0, uint16_t y_origin = 0, uint16_t x_length = 0, uint16_t y_length = 0, bool autoDrawActivated = false, bool touchActivated = true);
 
         bool touched(uint16_t x, uint16_t y);
         void draw();
 
-      protected:
+       protected:
         _displayObjectType& _display;
     };
 };
@@ -204,31 +193,31 @@ class drawIt
 //-----------------------------------------------------------------------------------------------------
 
 void drawIt::visuals::changeOrigin(uint16_t x_origin, uint16_t y_origin) {
-  _origin.x = x_origin, _origin.y = y_origin;
+    _origin.x = x_origin, _origin.y = y_origin;
 
-  if (_autoDraw) this->draw();
+    if (_autoDraw) this->draw();
 }
 
 void drawIt::visuals::changeLength(uint16_t x_length, uint16_t y_length) {
-  if (x_length >= 4 && y_length >= 4) {
-    _length.x = x_length, _length.y = y_length;
-  } else {
-    _length.x = 4, _length.y = 4;
-  }
+    if (x_length >= 4 && y_length >= 4) {
+        _length.x = x_length, _length.y = y_length;
+    } else {
+        _length.x = 4, _length.y = 4;
+    }
 
-  if (_autoDraw) this->draw();
+    if (_autoDraw) this->draw();
 }
 
 void drawIt::visuals::setVisibility(bool visible) {
-  _visible = visible;
+    _visible = visible;
 
-  if (_autoDraw) this->draw();
+    if (_autoDraw) this->draw();
 }
 
 void drawIt::visuals::autoDraw(bool active) {
-  _autoDraw = active;
+    _autoDraw = active;
 
-  if (_autoDraw) this->draw();
+    if (_autoDraw) this->draw();
 }
 
 
@@ -239,11 +228,11 @@ void drawIt::visuals::autoDraw(bool active) {
 //-----------------------------------------------------------------------------------------------------
 
 void drawIt::color::setBackgroundColor(uint16_t backgroundColor) {
-  _color.background = backgroundColor;
+    _color.background = backgroundColor;
 }
 
 void drawIt::color::setOutlineColor(uint16_t outlineColor) {
-  _color.outline = outlineColor;
+    _color.outline = outlineColor;
 }
 
 
@@ -254,94 +243,101 @@ void drawIt::color::setOutlineColor(uint16_t outlineColor) {
 //-----------------------------------------------------------------------------------------------------
 
 drawIt::slider::slider(_displayObjectType& _dsp, uint16_t x_origin, uint16_t y_origin, uint16_t x_length, uint16_t y_length, bool autoDrawActivated, float value, bool touchActivated)
-  : _display(_dsp) {
-  this->autoDraw(autoDrawActivated);
-  this->changeOrigin(x_origin, y_origin);
-  this->changeLength(x_length, y_length);
-  this->setValue(value);
-  this->setTouch(touchActivated);
-  this->setVisibility(true);
+    : _display(_dsp) {
+    this->autoDraw(autoDrawActivated);
+    this->changeOrigin(x_origin, y_origin);
+    this->changeLength(x_length, y_length);
+    this->setValue(value);
+    this->setTouch(touchActivated);
+    this->setVisibility(true);
 }
 
 void drawIt::slider::setValue(float value) {
-  if (value > 1.0) {
-    _value = 1.0;
-  } else if (value < 0) {
-    _value = 0;
-  } else {
-    _value = value;
-  }
-
-  if (_autoDraw) this->draw();
-}
-
-float drawIt::slider::getValue() {
-  return _value;
-}
-
-void drawIt::slider::setSliderBackgroundColor(uint16_t sliderBackgroundColor) {
-  _sliderColor.background = sliderBackgroundColor;
-}
-
-uint16_t drawIt::slider::getSliderBackgroundColor() {
-  return _sliderColor.background;
-}
-
-bool drawIt::slider::touched(uint16_t x, uint16_t y) {
-  //Touch on the slider?
-  if (_visible && (x >= _origin.x) && (y >= _origin.y) && (x <= (_origin.x + _length.x)) && (y <= (_origin.y + _length.y))) { // clear...
-    if (_length.x > _length.y) { //Wich direction?
-      //minimum = _length.y/2 = 0
-      //maximum = _length.x - _length.y/2 = 1
-
-      _value = (x - _origin.x - _length.y / 2.0) / (_length.x - _length.y) * 1.0;
-
-      if (_value > 1) _value = 1.0; else if (_value < 0) _value = 0;
-    } else if (_length.y > _length.x) {
-      //minimum = _length.y - _length.x/2 = 0
-      //maximum = _length.x/2 = 1
-
-      _value = 1.0 - (y - _origin.y - _length.x / 2.0) / (_length.y - _length.x) * 1.0;
-
-      if (_value > 1) _value = 1.0; else if (_value < 0) _value = 0;
-    } else { //If it isn't a slider but a box
-      _value = 0.0;
+    if (value > 1.0) {
+        _value = 1.0;
+    } else if (value < 0) {
+        _value = 0;
+    } else {
+        _value = value;
     }
 
     if (_autoDraw) this->draw();
+}
 
-    return true;
-  } else return false;
+float drawIt::slider::getValue() {
+    return _value;
+}
+
+void drawIt::slider::setSliderBackgroundColor(uint16_t sliderBackgroundColor) {
+    _sliderColor.background = sliderBackgroundColor;
+}
+
+uint16_t drawIt::slider::getSliderBackgroundColor() {
+    return _sliderColor.background;
+}
+
+bool drawIt::slider::touched(uint16_t x, uint16_t y) {
+    //Touch on the slider?
+    if (_visible && (x >= _origin.x) && (y >= _origin.y) && (x <= (_origin.x + _length.x)) && (y <= (_origin.y + _length.y))) {  // clear...
+        if (_length.x > _length.y) {                                                                                             //Wich direction?
+            //minimum = _length.y/2 = 0
+            //maximum = _length.x - _length.y/2 = 1
+
+            _value = (x - _origin.x - _length.y / 2.0) / (_length.x - _length.y) * 1.0;
+
+            if (_value > 1)
+                _value = 1.0;
+            else if (_value < 0)
+                _value = 0;
+        } else if (_length.y > _length.x) {
+            //minimum = _length.y - _length.x/2 = 0
+            //maximum = _length.x/2 = 1
+
+            _value = 1.0 - (y - _origin.y - _length.x / 2.0) / (_length.y - _length.x) * 1.0;
+
+            if (_value > 1)
+                _value = 1.0;
+            else if (_value < 0)
+                _value = 0;
+        } else {  //If it isn't a slider but a box
+            _value = 0.0;
+        }
+
+        if (_autoDraw) this->draw();
+
+        return true;
+    } else
+        return false;
 }
 
 void drawIt::slider::draw() {
-  if (_visible) {
-    //Outline/Background
-    _display.fillRect(_origin.x, _origin.y, _length.x, _length.y, _color.background);
-    _display.drawRect(_origin.x, _origin.y, _length.x, _length.y, _color.outline);
+    if (_visible) {
+        //Outline/Background
+        _display.fillRect(_origin.x, _origin.y, _length.x, _length.y, _color.background);
+        _display.drawRect(_origin.x, _origin.y, _length.x, _length.y, _color.outline);
 
-    //Slider
-    if (_length.x > _length.y) { //Horizontal
-      uint16_t _slide_length     = (_length.x - 2) - (_length.y - 2),
-               _x_sliderposition = (_origin.x + 1) + (_slide_length * _value),
-               _y_sliderposition = _origin.y + 1,
-               _sliderbox_size   = _length.y - 2;
+        //Slider
+        if (_length.x > _length.y) {  //Horizontal
+            uint16_t _slide_length = (_length.x - 2) - (_length.y - 2),
+                     _x_sliderposition = (_origin.x + 1) + (_slide_length * _value),
+                     _y_sliderposition = _origin.y + 1,
+                     _sliderbox_size = _length.y - 2;
 
-      _display.fillRect(_x_sliderposition, _y_sliderposition, _sliderbox_size, _sliderbox_size, _sliderColor.background);
-      _display.drawRect(_x_sliderposition, _y_sliderposition, _sliderbox_size, _sliderbox_size, _sliderColor.outline);
-    } else if (_length.x < _length.y) { //Vertical
-      uint16_t _slide_length     = (_length.y - 2) - (_length.x - 2),
-               _y_sliderposition = (_origin.y + 1) + (_slide_length * (1 - _value)),
-               _x_sliderposition = _origin.x + 1,
-               _sliderbox_size   = _length.x - 2;
+            _display.fillRect(_x_sliderposition, _y_sliderposition, _sliderbox_size, _sliderbox_size, _sliderColor.background);
+            _display.drawRect(_x_sliderposition, _y_sliderposition, _sliderbox_size, _sliderbox_size, _sliderColor.outline);
+        } else if (_length.x < _length.y) {  //Vertical
+            uint16_t _slide_length = (_length.y - 2) - (_length.x - 2),
+                     _y_sliderposition = (_origin.y + 1) + (_slide_length * (1 - _value)),
+                     _x_sliderposition = _origin.x + 1,
+                     _sliderbox_size = _length.x - 2;
 
-      _display.fillRect(_x_sliderposition, _y_sliderposition, _sliderbox_size, _sliderbox_size, _sliderColor.background);
-      _display.drawRect(_x_sliderposition, _y_sliderposition, _sliderbox_size, _sliderbox_size, _sliderColor.outline);
-    } else { //Same Same but different
-      _display.fillRect(_origin.x + 1, _origin.y + 1, _length.x - 2, _length.y - 2, _sliderColor.background);
-      _display.drawRect(_origin.x + 1, _origin.y + 1, _length.x - 2, _length.y - 2, _sliderColor.outline);
+            _display.fillRect(_x_sliderposition, _y_sliderposition, _sliderbox_size, _sliderbox_size, _sliderColor.background);
+            _display.drawRect(_x_sliderposition, _y_sliderposition, _sliderbox_size, _sliderbox_size, _sliderColor.outline);
+        } else {  //Same Same but different
+            _display.fillRect(_origin.x + 1, _origin.y + 1, _length.x - 2, _length.y - 2, _sliderColor.background);
+            _display.drawRect(_origin.x + 1, _origin.y + 1, _length.x - 2, _length.y - 2, _sliderColor.outline);
+        }
     }
-  }
 }
 
 
@@ -352,24 +348,27 @@ void drawIt::slider::draw() {
 //-----------------------------------------------------------------------------------------------------
 
 drawIt::button::button(_displayObjectType& _dsp, uint16_t x_origin, uint16_t y_origin, uint16_t x_length, uint16_t y_length, bool autoDrawActivated, bool touchActivated)
-  : _display(_dsp) {
-  this->autoDraw(autoDrawActivated);
-  this->changeOrigin(x_origin, y_origin);
-  this->changeLength(x_length, y_length);
-  this->setTouch(touchActivated);
-  this->setVisibility(true);
+    : _display(_dsp) {
+    this->autoDraw(autoDrawActivated);
+    this->changeOrigin(x_origin, y_origin);
+    this->changeLength(x_length, y_length);
+    this->setTouch(touchActivated);
+    this->setVisibility(true);
 }
 
 bool drawIt::button::touched(uint16_t x, uint16_t y) {
-  if (_visible && (x >= _origin.x) && (y >= _origin.y) && (x <= (_origin.x + _length.x)) && (y <= (_origin.y + _length.y))) return true; else return false;
+    if (_visible && (x >= _origin.x) && (y >= _origin.y) && (x <= (_origin.x + _length.x)) && (y <= (_origin.y + _length.y)))
+        return true;
+    else
+        return false;
 }
 
 void drawIt::button::draw() {
-  if (_visible) {
-    //Outline/Background (all of the button)
-    _display.fillRect(_origin.x, _origin.y, _length.x, _length.y, _color.background);
-    _display.drawRect(_origin.x, _origin.y, _length.x, _length.y, _color.outline);
-  }
+    if (_visible) {
+        //Outline/Background (all of the button)
+        _display.fillRect(_origin.x, _origin.y, _length.x, _length.y, _color.background);
+        _display.drawRect(_origin.x, _origin.y, _length.x, _length.y, _color.outline);
+    }
 }
 
-#endif //drawIt_h
+#endif  //drawIt_h
